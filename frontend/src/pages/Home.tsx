4f112c1edcae8eedee3c60ac063549cbecd94a8a
Nicholas Snow Mattsson <snownicholas2@gmail.com>
2025-05-30 18:40:42 +0200

    image sldier done

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,7 +1,12 @@
+import { ImageSlider } from "../components/ImageSlider";
+
 export const Home = () => {
-    return (
-        <>
-            Home
-        </>
-    )
-}
+  return (
+    <>
+      <div className="homepage_container">
+        <h1>ARE YOU READY TO ROCK YOUR SOCKS?</h1>
+        <ImageSlider></ImageSlider>
+      </div>
+    </>
+  );
+};
