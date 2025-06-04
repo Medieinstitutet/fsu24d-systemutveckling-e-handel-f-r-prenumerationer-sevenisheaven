baaceae3e05c0efea726b5bf2d89eaf58f9b41fd
ElinSabel <elin.sabel@live.se>
2025-06-04 10:58:26 +0300

    Just added some text

--- frontend/src/pages/OrderConfirmation.tsx
+++ frontend/src/pages/OrderConfirmation.tsx
@@ -1,7 +1,33 @@
+import { Star } from "lucide-react";
+
 export const OrderConfirmation = () => {
-    return (
-        <>
-            OrderConfirmation
-        </>
-    )
-}
+  const getStarColor = (level?: string) => {
+    switch (level) {
+      case "Sock Emergency":
+        return "#CD7F32";
+      case "Sock & Roll":
+        return "#C0C0C0";
+      case "Sock Royalty":
+        return "#FFD700";
+      default:
+        return "#cccccc"; // fallback
+    }
+  };
+  return (
+    <>
+      <h1>Order Confirmation</h1>
+      <h3>Thank You! {/* ${user.name} */}</h3>
+      <h3>
+        You have subscription {/* ${user.level} */}{" "}
+        <Star fill={getStarColor(/* levelName */)} />
+      </h3>
+      <p className="centered-p-element">
+        You will receive a confirmation email with all the details about your
+        new subscription. You can now log in with your email and password to
+        order your first pair of TCS Socks. At any time, you can cancel,
+        downgrade, or upgrade your subscription. To do so, simply log in to your
+        account.
+      </p>
+    </>
+  );
+};
