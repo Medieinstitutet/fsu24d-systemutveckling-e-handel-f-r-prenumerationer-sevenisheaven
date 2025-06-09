4b80a36a4463a0b748577f894943e92e2b65091b
Nicholas Snow Mattsson <snownicholas2@gmail.com>
2025-06-09 14:15:12 +0200

    failure mail and set subscription_status

--- backend/routes/stripe.ts
+++ backend/routes/stripe.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import {
   checkoutSessionEmbedded, getSession,
+  testFailedPayment,
   webhook,
 } from "../controllers/stripeController";
 const router = Router();
@@ -8,5 +9,6 @@ const router = Router();
 router.post("/create-checkout-session-embedded", checkoutSessionEmbedded);
 router.get("/sessions/:sessionId", getSession);
 router.post("/webhook", webhook);
+router.get("/webhook/test-failed-payment", testFailedPayment);
 
 export default router;
