87c749357e0dacc6a726dd6a253aac9299b3d04a
matterattetatte <matias.marti@medieinstitutet.se>
2025-06-11 09:21:52 +0200

    dynamic base path for navigation

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -8,7 +8,7 @@ function App() {
     <>
       <AuthProvider>
         <CartProvider>
-          <RouterProvider router={router}></RouterProvider>
+          <RouterProvider basename={import.meta.env.BASE_URL} router={router}></RouterProvider>
         </CartProvider>
       </AuthProvider>
     </>
